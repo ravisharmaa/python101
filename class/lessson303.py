3fcad3d82adc7b7f1f6bc0ff81d82c5d32b80fe1
ravi <ravi.sharma@javra.com>
2019-07-24 12:56:29 +0545

    classes 303

---
+++ class/lessson303.py
@@ -0,0 +1,19 @@
+class Joe(object):
+
+    greeting = 'Hello, Joe'
+
+    # whenever a function is defined in the python class
+    # the current instance is passed to it by default
+    # notice the self key word in the function which
+    # is not sent through tbe function, but throws an
+    # error when not removed from the definition inside the class
+    # Because of this automatic passing of the instance instance methods are called "bound" methods.
+
+    def call_me_from_the_class(self):
+        print('calling the function')
+
+
+joe_obj = Joe()
+print(joe_obj.greeting)
+
+joe_obj.call_me_from_the_class()
